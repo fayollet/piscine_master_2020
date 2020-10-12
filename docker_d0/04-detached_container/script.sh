3b8a83708b707854dbc4f2dbd870390035b01c56
SCcagg5 <eliot@LAPTOP-C7G3T243.localdomain>
2020-10-13 01:44:00 +0200

    merge

--- docker_d0/04-detached_container/script.sh
+++ docker_d0/04-detached_container/script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -d -p 8000:8000 php:7.3-cli php -S 0.0.0.0:8000
+docker run -d php:7.3-cli php -S 0.0.0.0:8000
--- docker_d0/05-multiple_containers/script.sh
+++ docker_d0/05-multiple_containers/script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run -d --name elegant_tony -e MYSQL_ROOT_PASSWORD=test mysql:latest ; 
-docker run -d --name dazzling_robin httpd:latest; 
+docker run -d --name elegant_tony -e MYSQL_ROOT_PASSWORD=test mysql:latest 
+docker run -d --name dazzling_robin httpd:latest
 docker run -d --name ginger_jeremie php:7.3-cli php -S 0.0.0.0:8080
